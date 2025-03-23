4402411146ab4833be70d63454c381fedf2050c0
danieledu007 <danielguillen274@gmail.com>
2025-03-23 10:31:19 +0100

    tolte parti innecesarie del file python

--- algoritmi.py
+++ algoritmi.py
@@ -1,12 +1,5 @@
 import random
 import time
-#def lista_random(n):
-#   limite=n
-#   for a in range(i,limite):
-#    a=[]
-#    return list
-#    ra=random.randint(1,1000)
-#    a.append(ra)
 def selection_sort(lista,len):
     for i in range(len):
         min=i
@@ -29,8 +22,7 @@ def insertion_sort(lista):
         j -= 1
         lista[j+1] = valore
     return lista
-#def merge_sort(lista):
-#def merge()
+
 a=[2,4,6,1,5]
 length= len(a)
 start=0
@@ -39,15 +31,8 @@ n=int(input("lunghezza della lista:"))
 li=[random.sample(range(n),n)]
 #for i in range(n):
 li=random.sample(range(n),n)
-#li.append(random.sample(range(100),n))
-#ra=random.randint(0,1000)
-#li.append(ra)
-#lista_random(n)
 start=time.time()
 selection_sort(li,n)
 end=time.time()
 print((end-start)*1000)
 #print ( bubble_sort(a))
-
-#print(length)
-# what?
